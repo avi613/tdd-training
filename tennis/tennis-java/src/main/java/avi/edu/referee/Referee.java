cc0a2eee7cc9a52bcc6996994737518919dc426d
avi613 <avi.elkharrat@gmail.com>
2016-10-12 23:38:02 +0200

    refactor isGameWon

--- tennis/tennis-java/src/main/java/avi/edu/referee/Referee.java
+++ tennis/tennis-java/src/main/java/avi/edu/referee/Referee.java
@@ -5,11 +5,8 @@ import avi.edu.speaker.WeirdTennisPoints;
 import avi.edu.player.score.Score;
 
 public class Referee {
-    // And God created brms...
     public void establishScore(Player winner, Player looser) {
-        if ((winner.getScore().getTrackPoints() == 3 && looser.getScore().getTrackPoints() < 3)
-                || (winner.getScore().getTrackPoints() > 3 && looser.getScore().getTrackPoints() >= 3 && winner.getScore().getTrackPoints() > looser.getScore().getTrackPoints())) {
-            // game won
+        if (isGameWon(winner, looser)) {
             winner.setScore(new Score(
                     0,
                     winner.getScore().getNumberOfGamesWon() + 1));
@@ -18,7 +15,6 @@ public class Referee {
                     looser.getScore().getNumberOfGamesWon()));
         }
         else
-            // point won
             winner.setScore(new Score(
                     winner.getScore().getTrackPoints() + 1,
                     winner.getScore().getNumberOfGamesWon()));
@@ -35,5 +31,10 @@ public class Referee {
                 || (player1.getScore().getNumberOfGamesWon() == 6 && player2.getScore().getNumberOfGamesWon() == 7))
             return true;
         return false;
+    }
+
+    private boolean isGameWon(Player winner, Player looser) {
+        return (winner.getScore().getTrackPoints() == 3 && looser.getScore().getTrackPoints() < 3)
+                || (winner.getScore().getTrackPoints() > 3 && looser.getScore().getTrackPoints() >= 3 && winner.getScore().getTrackPoints() > looser.getScore().getTrackPoints());
     }
 }
