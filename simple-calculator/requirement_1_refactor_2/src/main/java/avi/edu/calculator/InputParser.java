1e678bfe0c08a81f907612ad232a08dd147e0d62
avi613 <avi.elkharrat@gmail.com>
2016-11-23 23:31:30 +0100

    balanced abstreaction

--- simple-calculator/requirement_1_refactor_2/src/main/java/avi/edu/calculator/InputParser.java
+++ simple-calculator/requirement_1_refactor_2/src/main/java/avi/edu/calculator/InputParser.java
@@ -7,11 +7,25 @@ import static java.util.Arrays.stream;
 
 public class InputParser {
     public Operation parseToOperation(String input) {
-        if (input.split(",").length > 2) throw new IllegalArgumentException("Cannot process more than 2 numbers");
+        if (numberOfOperandsIsGreaterThanTwo(input))
+            throw new IllegalArgumentException("Cannot process more than 2 numbers");
+
         return new Operation(
                 input.trim().isEmpty()
-                        ? new int[]{0}
-                        : stream(input.split(",")).map(String::trim).mapToInt(Integer::parseInt).toArray(),
+                        ? zeroArray()
+                        : operandsArray(input),
                 new Addition());
+    }
+
+    private boolean numberOfOperandsIsGreaterThanTwo(String input) {
+        return input.split(",").length > 2;
+    }
+
+    private int[] zeroArray() {
+        return new int[]{0};
+    }
+
+    private int[] operandsArray(String input) {
+        return stream(input.split(",")).map(String::trim).mapToInt(Integer::parseInt).toArray();
     }
 }
